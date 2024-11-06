86a1c6c964c9af001ab4b7ba5a176d939b1b9443
AzhraMeisa09 <azhrameisa@gmail.com>
2024-11-06 13:26:21 +0700

    Login

--- app/src/main/java/com/example/proksi_tbptb/ui/theme/theme/Theme.kt
+++ app/src/main/java/com/example/proksi_tbptb/ui/theme/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.proksi_tbptb.ui.theme
+package com.example.proksi_tbptb.ui.theme.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/example/proksi_tbptb/ui/theme/theme/Type.kt
+++ app/src/main/java/com/example/proksi_tbptb/ui/theme/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.proksi_tbptb.ui.theme
+package com.example.proksi_tbptb.ui.theme.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
