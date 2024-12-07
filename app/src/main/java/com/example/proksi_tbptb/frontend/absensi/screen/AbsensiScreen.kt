82e4ebf8142e2d2260ccc42d1c2e9299346fac2d
Rania Shofi <raniashofim13@gmail.com>
2024-12-07 22:40:50 +0700

    Absensi Global

--- app/src/main/java/com/example/proksi_tbptb/frontend/absensi/screen/AbsensiScreen.kt
+++ app/src/main/java/com/example/proksi_tbptb/frontend/absensi/screen/AbsensiScreen.kt
@@ -2,7 +2,15 @@ package com.example.proksi_tbptb.frontend.absensi.screen
 
 import android.content.Context
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -15,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.proksi_tbptb.R
@@ -24,7 +33,6 @@ import com.example.proksi_tbptb.frontend.absensi.component.BoxAbsensi
 import com.example.proksi_tbptb.frontend.absensi.component.CustomButtonAbsensi
 import com.example.proksi_tbptb.frontend.absensi.component.CustomButtonWithIcon
 import com.example.proksi_tbptb.frontend.absensi.viewmodel.AbsensiViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun AbsensiScreen(
@@ -74,7 +82,9 @@ fun AbsensiScreen(
                     )
                     CustomButtonAbsensi(
                         text = "Global",
-                        onClick = { println("Global diklik") },
+                        onClick = {
+                            navController.navigate("all-kegiatan")
+                        },
                         modifier = Modifier.weight(1f),
                         buttonColor = Color(0xFFF5E7C6),
                         contentColor = Color.Black
